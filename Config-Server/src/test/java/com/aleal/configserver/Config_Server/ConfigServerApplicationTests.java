f6ec4225ac4a50158eb90ceb39a44991089aaf61
noerelo <noelia.reyeslopez@capgemini.com>
2026-06-02 08:49:27 +0200

    Primer commit

---
+++ Config-Server/src/test/java/com/aleal/configserver/Config_Server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleal.configserver.Config_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
