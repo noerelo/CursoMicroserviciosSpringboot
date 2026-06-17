c8a8a9ae5e59345e132dc33b4ec397cfa9a94d9f
noerelo <noelia.reyeslopez@capgemini.com>
2026-06-17 11:03:16 +0200

    Gateway

--- gatewayserver/src/main/java/com/aleal/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/aleal/gatewayserver/GatewayserverApplication.java
@@ -2,6 +2,11 @@ package com.aleal.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -10,4 +15,24 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/applications/apiHotel/**")
+						.filters(f -> f.rewritePath("/applications/apiHotel/(?<segment>.*)", "/${segment}")
+								.addRequestHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://HOTELS"))
+				.route(p -> p
+						.path("/applications/apiRooms/**")
+						.filters(f -> f.rewritePath("/applications/apiRooms/(?<segment>.*)", "/${segment}")
+								.addRequestHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://ROOMS"))
+				.route(p -> p
+						.path("/applications/apiReservations/**")
+						.filters(f -> f.rewritePath("/applications/apiReservations/(?<segment>.*)", "/${segment}")
+								.addRequestHeader("X-Response-Time", new Date().toString()))
+						.uri("lb://RESERVATIONS"))
+				.build();
+	}
 }
